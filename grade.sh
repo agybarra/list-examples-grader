801057abd07d46f11d9490be68fec268203978d8
agybarra <156254451+agybarra@users.noreply.github.com>
2024-02-14 15:50:05 -0800

    ah

--- grade.sh
+++ grade.sh
@@ -14,3 +14,41 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "file found!"
+else 
+    echo "file not found!"
+fi
+
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "Compilation succesful"
+else
+    echo "Compilation non successful"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+
+if [[$(grep -q "OK")]]
+then 
+    echo "All tests passed!"
+    exit
+fi 
+
+TOTALTESTRUN=$(grep -Eo "Tests run: [0-9]+" junit-output.txt | grep -Eo "[0-9]+")
+
+TOTALFAIL=$(grep -Eo "Failures: [0-9]+" junit-output.txt | grep -Eo "[0-9]+")
+
+TOTALSUCCESS=$((TOTALTESTRUN-TOTALFAIL))
+
+echo " Your grade is $TOTALSUCCESS / $TOTALTESTRUN "
